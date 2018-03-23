9712d8dc7c218059758ebf3f395dc302492c677f
jenisonleo <jenisongracious@gmail.com>
2018-03-23 13:25:13 +0530

    inital push

--- app/src/main/java/app/leo/jenison/com/snapper/MainActivity.java
+++ app/src/main/java/app/leo/jenison/com/snapper/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.LinearSnapHelper;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SnapHelper;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RecyclerView recyclerView=findViewById(R.id.recycerlview);
+        final RecyclerView recyclerView=findViewById(R.id.recycerlview);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new RecycerlAdapter());
         SnapHelper snapHelper=new CustomLinearHelper();
         snapHelper.attachToRecyclerView(recyclerView);
+        findViewById(R.id.up).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recyclerView.smoothScrollToPosition(((LinearLayoutManager)recyclerView.getLayoutManager()).findFirstVisibleItemPosition()+10);
+            }
+        });
+        findViewById(R.id.down).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recyclerView.smoothScrollToPosition(((LinearLayoutManager)recyclerView.getLayoutManager()).findFirstVisibleItemPosition()-10);
+            }
+        });
     }
 }
